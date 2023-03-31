41a32f762a77ad618c9c4ff511afa4a25fb73e86
enrique palomino <octepc.97@gmail.com>
2023-03-31 08:47:50 -0500

    feat: :sparkles: Made the actions for the useReducer hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,76 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { type Action, type State } from './types'
+import { useReducer } from 'react'
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer (state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      result: action.payload
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: true,
+      result: action.payload
+    }
+  }
+  return state
+}
 
 function App () {
-  const initialState = {
-    fromLanguage: 'auto',
-    toLanguage: 'en',
-    fromText: '',
-    result: '',
-    loading: false
-  }
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+  }, dispatch] = useReducer(reducer, initialState)
 
-  function reducer (state, action) {
-    const { type, payload } = action
-
-    if (type === 'INTERCHANGE_LANGUAGES') {
-      return {
-        ...state,
-        fromLanguage: state.toLanguage,
-        toLanguage: state.fromLanguage
-      }
-    }
-
-    if (type === 'SET_FROM_LANGUAGE') {
-      return {
-        ...state,
-        fromLanguage: payload
-      }
-    }
-
-    if (type === 'SET_TO_LANGUAGE') {
-      return {
-        ...state,
-        toLanguage: payload
-      }
-    }
-
-    if (type === 'SET_FROM_TEXT') {
-      return {
-        ...state,
-        loading: true,
-        result: payload
-      }
-    }
-
-    if (type === 'SET_RESULT') {
-      return {
-        ...state,
-        loading: true,
-        result: payload
-      }
-    }
-    return state
-  }
+  console.log({ fromLanguage })
 
   return (
     <div className="App">
       <h1>Google Translate</h1>
+      <button onClick={() => {
+        dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
+      }}>Cambiar a Español</button>
     </div>
   )
 }
