bddc1b308514cb28ae04b1be11f7ac77a762da70
enrique palomino <octepc.97@gmail.com>
2023-03-30 21:31:10 -0500

    feat: :sparkles: Made the useReducer workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,57 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App () {
+  const initialState = {
+    fromLanguage: 'auto',
+    toLanguage: 'en',
+    fromText: '',
+    result: '',
+    loading: false
+  }
+
+  function reducer (state, action) {
+    const { type, payload } = action
+
+    if (type === 'INTERCHANGE_LANGUAGES') {
+      return {
+        ...state,
+        fromLanguage: state.toLanguage,
+        toLanguage: state.fromLanguage
+      }
+    }
+
+    if (type === 'SET_FROM_LANGUAGE') {
+      return {
+        ...state,
+        fromLanguage: payload
+      }
+    }
+
+    if (type === 'SET_TO_LANGUAGE') {
+      return {
+        ...state,
+        toLanguage: payload
+      }
+    }
+
+    if (type === 'SET_FROM_TEXT') {
+      return {
+        ...state,
+        loading: true,
+        result: payload
+      }
+    }
+
+    if (type === 'SET_RESULT') {
+      return {
+        ...state,
+        loading: true,
+        result: payload
+      }
+    }
+    return state
+  }
+
   return (
     <div className="App">
       <h1>Google Translate</h1>
